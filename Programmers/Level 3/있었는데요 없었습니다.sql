f785a95dade4ca08e2dab260b5f4d32fd222397c
vividswan <vividswan@naver.com>
2020-08-24 14:10:47 +0900

    Update Location .sql File

---
+++ Programmers/Level 3/있었는데요 없었습니다.sql
@@ -0,0 +1,1 @@
+select animal_ins.animal_id, animal_ins.name from animal_ins, animal_outs where animal_ins.animal_id = animal_outs.animal_id && animal_ins.datetime > animal_outs.datetime order by animal_ins.datetime asc;
