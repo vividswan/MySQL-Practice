cd98c58ab5eddd9b5e1fd9c238bef4249019cf91
vividswan <vividswan@naver.com>
2020-09-08 11:33:02 +0900

    Update NULL 처리하기.sql

--- Programmers/Level 2/NULL 처리하기.sql
+++ Programmers/Level 2/NULL 처리하기.sql
@@ -1,1 +1,1 @@
-SELECT animal_type, ifnull(name,'No name'), sex_upon_intake from animal_ins order by animal_id;
+SELECT ANIMAL_TYPE, IFNULL(NAME,'No name'), SEX_UPON_INTAKE FROM ANIMAL_INS ORDER BY ANIMAL_ID;
