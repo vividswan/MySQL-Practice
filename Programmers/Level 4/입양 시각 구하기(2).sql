46af3010f3f9efb632ee1c2be5eca3bdce7bb392
vividswan <vividswan@naver.com>
2021-01-03 02:08:06 +0900

    Update 입양 시각 구하기(2).sql

--- Programmers/Level 4/입양 시각 구하기(2).sql
+++ Programmers/Level 4/입양 시각 구하기(2).sql
@@ -1,7 +1,5 @@
-set @hour = -1;
-select (@hour := @hour+1) as 'HOUR',
-(
-    select count(datetime) from animal_outs where hour(datetime) = @hour
-) as 'COUNT'
-from animal_outs
-where @hour < 23;
+SET @hour = -1;
+SELECT (@hour := @hour+1) as HOUR,
+(SELECT COUNT(DATETIME) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) as COUNT
+FROM ANIMAL_OUTS
+WHERE @hour < 23;
